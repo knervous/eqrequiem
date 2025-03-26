0948e99a23d6a7229f65e7d517844c40f2500a88
Paul Johnson <pjohnson@minitab.de>
2025-03-26 07:32:20 -0400

    read jwt

---
+++ client/src/auth.ts
@@ -0,0 +1,23 @@
+export function getCookie(name: string) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+  
+export function parseJwt(token: string) : object | undefined {
+    try {
+      const base64Url = token.split('.')[1]; // Get the payload (second part)
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Convert base64url to base64
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split('')
+          .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+          .join('')
+      );
+  
+      return JSON.parse(jsonPayload);
+    } catch (e) {
+      console.error('Failed to parse JWT:', e);
+      return undefined;
+    }
+  }
--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import DiscordIcon from "./discord";
+import { getCookie, parseJwt } from "../auth";
 
 const DISCORD_CLIENT_ID = "1354327280532459582";
 const url = import.meta.env.DEV ? "https://localhost:3500/api/login" : "https://requiem-jade.vercel.app/api/login";
@@ -8,8 +9,19 @@ const REDIRECT_URI = encodeURIComponent(url); // your registered callback URL
 const RESPONSE_TYPE = "code";
 const SCOPE = encodeURIComponent("identify"); // add or remove scopes as needed
 
+  
+const token = getCookie('auth_token');
+let decoded: object;
+if (token) {
+  decoded = parseJwt(token) as object;
+}
+
 const DiscordLoginButton = () => {
   const handleLogin = () => {
+    if (decoded) {
+      console.log('Wanted to log in')
+      return;
+    }
     const discordAuthUrl = `https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`
     window.location.href = discordAuthUrl;
   };
@@ -22,7 +34,7 @@ const DiscordLoginButton = () => {
       onClick={handleLogin}
       startIcon={<DiscordIcon />}
     >
-      Login with Discord
+      {decoded ? `Continue as ${decoded.username}` : 'Login with Discord'}
     </Button>
   );
 };
