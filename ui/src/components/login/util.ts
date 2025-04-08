33bce03d4d18a27c8c38f0838c6d7177c6529aab
Paul Johnson <pjohnson@minitab.de>
2025-04-08 04:20:26 -0400

    headers and prod site

--- ui/src/components/login/util.ts
+++ ui/src/components/login/util.ts
@@ -1,5 +1,5 @@
 export const DISCORD_CLIENT_ID = "1354327280532459582";
-export const url = import.meta.env.DEV ? "https://localhost:3500/login" : "https://requiem-jade.vercel.app/login";
+export const url = import.meta.env.DEV ? "https://localhost:3500/login" : "https://eqrequiem.com/login";
 export const REDIRECT_URI = encodeURIComponent(url); // your registered callback URL
 export const RESPONSE_TYPE = "code";
 export const SCOPE = encodeURIComponent("identify"); // add or remove scopes as needed
