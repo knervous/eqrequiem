10367e83f3d40b18843fc00978a94c0bd1aacec6
Paul Johnson <pjohnson@minitab.de>
2025-05-06 05:49:16 -0400

    add case for deserialize

--- server/internal/session/session_bench_test.go
+++ server/internal/session/session_bench_test.go
@@ -5,6 +5,7 @@ import (
 
 	eq "github.com/knervous/eqgo/internal/api/capnp"
 	"github.com/knervous/eqgo/internal/api/opcodes"
+	"github.com/stretchr/testify/assert"
 )
 
 // noopMessenger satisfies ClientMessenger but does nothing.
@@ -15,11 +16,25 @@ func (noopMessenger) SendStream(sessionID int, data []byte) error   { return nil
 
 var benchSession *Session
 var benchMessage eq.JWTResponse
+var buffer []byte
 
 func init() {
 	benchSession = NewSessionManager().CreateSession(noopMessenger{}, 42, "127.0.0.1")
 	benchMessage, _ = NewMessage(benchSession, eq.NewRootJWTResponse)
 	benchMessage.SetStatus(1)
+	buffer = []byte{0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}
+
+}
+
+func BenchmarkDeserialize(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		jwtResponse, err := eq.Deserialize(buffer, eq.ReadRootJWTResponse)
+		if err != nil {
+			b.Fatal(err)
+		}
+		assert.Equal(b, int32(1), jwtResponse.Status())
+	}
 }
 
 func BenchmarkSendData(b *testing.B) {
